0055905dd4f25e5b76141b3ec27a126616268c0a
David Taylor <david@taylorhq.com>
2021-01-29 12:43:23 +0000

    ultravisor: for exception during castcall method, notify caller
    Previously the caller would never receive any notification that the
    child had failed, and would block forever. This commit treats a failure
    during the call the same way as a failure while the call is queuing.

--- ultravisor/lib/ultravisor/child/call.rb
+++ ultravisor/lib/ultravisor/child/call.rb
@@ -8,6 +8,10 @@ class Ultravisor::Child::Call
 
   def go!(receiver)
     @rv_q << receiver.__send__(@method_name, *@args, &@blk)
+  rescue Exception => ex
+    @rv_q << @rv_fail
+    raise
+  ensure
     @rv_q.close
   end
 
