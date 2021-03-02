9ac3ed2e90ecef11fe229f7ba470b560cfd133bd
David Taylor <david@taylorhq.com>
2021-03-02 16:11:16 +0000

    FIX: Never block waiting for loggerstash worker (#10)
    logstash_writer will be nil if the logstash_writer worker is not running
    Ultravisor's restart policy ensures this will never happen at runtime. But
    it does happen during startup and shutdown. In this case, we want to skip
    writing to logstash, not block forever. STDOUT logging will continue.

--- lib/service_skeleton/ultravisor_loggerstash.rb
+++ lib/service_skeleton/ultravisor_loggerstash.rb
@@ -4,8 +4,16 @@ module ServiceSkeleton
   module UltravisorLoggerstash
     def logstash_writer
       #:nocov:
-      @ultravisor[:logstash_writer].unsafe_instance
+      @ultravisor[:logstash_writer].unsafe_instance(wait: false)
       #:nocov:
+    end
+
+    # logstash_writer will be nil if the logstash_writer worker is not running
+    # Ultravisor's restart policy ensures this will never happen at runtime. But
+    # it does happen during startup and shutdown. In this case, we want to skip
+    # writing to logstash, not block forever. STDOUT logging will continue.
+    def loggerstash_log_message(*args)
+      super if !logstash_writer.nil?
     end
   end
 end
